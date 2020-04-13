045e2e5c3f23b2ebf4ec689a59c296175520dc73
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-04-13 15:11:29 +0530

    snake class template

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+from tkinter import *
+import math
+import random
+
+
+class cube(object):
+    rows = 0
+    w = 0
+
+    def __init__(self, start, dirx=1, diry=0, color=(255, 0, 0)):
+        pass
+
+    def move(self, dirx, diry):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+
+class snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+    
